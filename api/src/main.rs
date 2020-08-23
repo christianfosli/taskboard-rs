44ed12e7cef6b177bde942e5dc68d7bd7b1d3fec
Christian Fosli <christian.fosli@capgemini.com>
2020-08-23 12:03:57 +0200

    Add /healthz route

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,14 @@
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
 
 #[tokio::main]
 async fn main() {
     println!("todo-api starting up...");
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let health = warp::path!("healthz").and_then(health_handler);
+    let routes = hello.or(health);
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+}
 
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-
-    warp::serve(hello)
-        .run(([0, 0, 0, 0], 8000))
-        .await;
+async fn health_handler() -> Result<impl Reply, Rejection> {
+    Ok("OK")
 }

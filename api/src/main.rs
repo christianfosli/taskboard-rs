c3d12550c0ed275534ae4a088892c9939c0090e3
Christian Fosli <christian.fosli@capgemini.com>
2020-08-30 18:22:06 +0200

    Implement dummy endpoints for API

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,17 +1,17 @@
-use warp::{Filter, Rejection, Reply};
+use warp::Filter;
 #[macro_use]
 extern crate log;
+mod handlers;
+mod routes;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     info!("api starting...");
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    let health = warp::path!("healthz").and_then(health_handler);
-    let routes = hello.or(health).with(warp::log("taskboard_api"));
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
-}
 
-async fn health_handler() -> Result<impl Reply, Rejection> {
-    Ok("OK")
+    let routes = routes::task_routes()
+        .or(routes::health_check_route())
+        .with(warp::log("taskboard_api"));
+
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
---
+++ api/src/routes.rs
@@ -0,0 +1,113 @@
+use crate::handlers::{
+    health::handle_health, task_create::handle_task_create, task_get::handle_task_get,
+    task_list::handle_task_list, task_update::handle_task_update,
+};
+use warp::{Filter, Rejection, Reply};
+
+pub fn health_check_route() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    warp::path!("healthz").and_then(handle_health)
+}
+
+pub fn task_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    let get = warp::path!("task" / String)
+        .and(warp::get())
+        .and_then(handle_task_get);
+
+    let create = warp::path!("task")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and_then(handle_task_create);
+
+    let update = warp::path!("task")
+        .and(warp::put())
+        .and(warp::body::json())
+        .and_then(handle_task_update);
+
+    let get_for_project = warp::path!("project-tasks" / String)
+        .and(warp::get())
+        .and_then(handle_task_list);
+
+    get.or(create).or(update).or(get_for_project)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use taskboard_core_lib::{Status, Task};
+    use warp::hyper::StatusCode;
+
+    #[tokio::test]
+    async fn health_check_should_be_ok() {
+        let route = health_check_route();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/healthz")
+            .reply(&route)
+            .await;
+
+        assert_eq!(StatusCode::OK, res.status());
+    }
+
+    #[tokio::test]
+    async fn get_task_should_be_ok() {
+        let routes = task_routes();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/task/some-dummy-id")
+            .reply(&routes)
+            .await;
+
+        assert_eq!(StatusCode::OK, res.status());
+    }
+
+    #[tokio::test]
+    async fn create_task_should_create() {
+        let routes = task_routes();
+
+        let res = warp::test::request()
+            .method("POST")
+            .path("/task")
+            .json(&Task {
+                title: "created test-task".into(),
+                status: Status::Todo,
+                remaining_work: None,
+            })
+            .reply(&routes)
+            .await;
+
+        assert_eq!(StatusCode::CREATED, res.status());
+    }
+
+    #[tokio::test]
+    async fn update_task_should_be_ok() {
+        let routes = task_routes();
+
+        let res = warp::test::request()
+            .method("PUT")
+            .path("/task")
+            .json(&Task {
+                title: "updated test-task".into(),
+                status: Status::Doing,
+                remaining_work: Some(5),
+            })
+            .reply(&routes)
+            .await;
+
+        assert_eq!(StatusCode::OK, res.status());
+    }
+
+    #[tokio::test]
+    async fn get_for_project_should_be_ok() {
+        let routes = task_routes();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/project-tasks/some-project-id")
+            .reply(&routes)
+            .await;
+
+        assert_eq!(StatusCode::OK, res.status());
+    }
+}

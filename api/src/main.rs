b4c4766f5fdaff80c77a33f309009617bc539958
Christian Fosli <cfosli@gmail.com>
2020-08-30 12:00:24 +0200

    Add logging to api (#11)

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,14 @@
 use warp::{Filter, Rejection, Reply};
+#[macro_use]
+extern crate log;
 
 #[tokio::main]
 async fn main() {
-    println!("todo-api starting up...");
+    pretty_env_logger::init();
+    info!("api starting...");
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     let health = warp::path!("healthz").and_then(health_handler);
-    let routes = hello.or(health);
+    let routes = hello.or(health).with(warp::log("taskboard_api"));
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
 

642cc5573eaa7d728bacc6d9e839fa38db0473c3
Christian Fosli <cfosli@gmail.com>
2020-09-18 22:23:22 +0200

    Fetch from back-end (#16)
    * Updates frontend to fetch tasks from backend.
    * Configures CORS for backend (just allow any)

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,8 +9,13 @@ async fn main() {
     pretty_env_logger::init();
     info!("api starting...");
 
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET", "POST", "PUT", "DELETE"]);
+
     let routes = routes::task_routes()
         .or(routes::health_check_route())
+        .with(cors)
         .with(warp::log("taskboard_api"));
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
--- app/src/app.rs
+++ app/src/app.rs
@@ -1,29 +1,69 @@
 use crate::components::taskbox::TaskBox;
-use taskboard_core_lib::{Status, Task};
+use taskboard_core_lib::{ProjectTasks, Task};
 use yew::{
+    format::Json,
+    format::Nothing,
     prelude::*,
+    services::fetch::FetchTask,
+    services::fetch::Request,
+    services::fetch::Response,
     services::{ConsoleService, FetchService},
 };
+
+const API_URL: Option<&'static str> = option_env!("API_URL");
 
 pub struct Model {
     link: ComponentLink<Self>,
     project_id: String,
     project_title: String,
-    tasks: Vec<Task>,
-    loading: bool,
+    tasks: Option<Vec<Task>>,
+    ft: Option<FetchTask>,
+    fetch_status: FetchStatus,
+}
+
+enum FetchStatus {
+    Loading,
+    Completed,
+    Failed,
 }
 
 pub enum Msg {
-    Add,
+    Add(String),
     Update(Task),
     Delete(Task),
+    FetchCompleted(ProjectTasks),
+    FetchFailed,
 }
 
 impl Model {
     fn fetch_tasks(&mut self) {
-        ConsoleService::log("fetching tasks...");
-        self.loading = true;
-        // todo: fetch data
+        self.fetch_status = FetchStatus::Loading;
+
+        match API_URL {
+            Some(url) => {
+                let req = Request::get(&format!("{}/project-tasks/{}", url, self.project_id))
+                    .body(Nothing)
+                    .unwrap();
+
+                let callback = self.link.callback(
+                    |res: Response<Json<Result<ProjectTasks, anyhow::Error>>>| {
+                        if let (meta, Json(Ok(body))) = res.into_parts() {
+                            if meta.status.is_success() {
+                                return Msg::FetchCompleted(body);
+                            }
+                        }
+                        Msg::FetchFailed
+                    },
+                );
+
+                self.ft = Some(FetchService::fetch(req, callback).unwrap());
+            }
+            None => ConsoleService::error("Unable to fetch tasks because API_URL is not set"),
+        }
+    }
+
+    fn add_task(&mut self, title: &str) {
+        ConsoleService::log(&format!("Should be adding task {}", title))
     }
 }
 
@@ -35,16 +75,26 @@ impl Component for Model {
             link,
             project_id: String::from("tmp-id"), // TODO - get from query param
             project_title: String::from("tmp"),
-            tasks: Vec::new(),
-            loading: true,
+            tasks: None,
+            ft: None,
+            fetch_status: FetchStatus::Loading,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::Add => ConsoleService::log("should be adding task"),
+            Msg::Add(title) => self.add_task(&title),
             Msg::Update(task) => ConsoleService::log(&format!("Should be updating {}", task.title)),
             Msg::Delete(task) => ConsoleService::log(&format!("Should be deleting {}", task.title)),
+            Msg::FetchCompleted(tasks) => {
+                self.tasks = Some(tasks.tasks);
+                self.fetch_status = FetchStatus::Completed;
+                return true;
+            }
+            Msg::FetchFailed => {
+                self.fetch_status = FetchStatus::Failed;
+                return true;
+            }
         }
         false
     }
@@ -54,23 +104,42 @@ impl Component for Model {
         false
     }
 
-    fn rendered(&mut self, _first_render: bool) {
+    fn rendered(&mut self, first_render: bool) {
         ConsoleService::log("App rendered");
-        self.fetch_tasks();
+        if first_render {
+            self.fetch_tasks();
+        }
     }
 
     fn view(&self) -> Html {
-        let dummy_task = Task {
-            title: String::from("Dummy"),
-            status: Status::Todo,
-            remaining_work: Some(5),
+        let to_taskbox = |model: &Task| {
+            html! {
+                <TaskBox onchange=self.link.callback(|x| Msg::Update(x)) data=model />
+            }
         };
+
+        let task_list = match self.tasks.clone() {
+            Some(tasks) => html! {
+                <ul>
+                {tasks.iter().map(|t| to_taskbox(t)).collect::<Html>()}
+                </ul>
+            },
+            None => html! {
+                <ul></ul>
+            },
+        };
+
+        let status_message = match self.fetch_status {
+            FetchStatus::Loading => "Loading...",
+            FetchStatus::Completed => "",
+            FetchStatus::Failed => "Failed to fetch tasks 😭",
+        };
+
         html! {
             <main>
-                <h1>{ &format!("Project {} - Board", self.project_title) }</h1>
-                <ul>
-                    <TaskBox onchange=self.link.callback(|task| Msg::Update(task)) data=&dummy_task/>
-                </ul>
+            <h1>{ &format!("Taskboard for {}", self.project_title) }</h1>
+            <div class="status-msg">{ status_message }</div>
+            {task_list}
             </main>
         }
     }

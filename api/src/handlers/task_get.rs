0a3a2dd8a84ddddc4327c56028b05969534a57d8
Christian Fosli <cfosli@gmail.com>
2020-09-27 10:21:21 +0200

    API & Commands (#18)
    * Fix typo wrong type for project_id in core-lib
    
    * Update API with commands from new core-lib

--- api/src/handlers/task_get.rs
+++ api/src/handlers/task_get.rs
@@ -4,5 +4,5 @@ use warp::Reply;
 
 pub async fn handle_task_get(id: String) -> Result<impl Reply, Infallible> {
     info!("Pretending to retrieve task with id {} from ES", id);
-    Ok(warp::reply::json(&Task::new("dummy")))
+    Ok(warp::reply::json(&Task::new(5, "Learn to write rust")))
 }

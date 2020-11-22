1e0375ece79159903a9979db61f9b5f482c890c4
Christian Fosli <christian.fosli@capgemini.com>
2020-11-22 17:15:56 +0100

    Switch nginx-nigress to helm version
    This should hopefully make it easier to add config for monitoring

--- infra/k8s/install-ingress-nginx.sh
+++ infra/k8s/install-ingress-nginx.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/cloud/deploy.yaml
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx

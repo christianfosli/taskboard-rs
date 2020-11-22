e175085229f7bc8fb7535844bd00444eafc496b0
Christian Fosli <christian.fosli@capgemini.com>
2020-11-22 18:49:58 +0100

    Monitor ingress with prometheus
    * Update ingress-nginx to expose metrics
    
    * Add ServiceMonitor so that prometheus uses those metrics

--- infra/k8s/install-ingress-nginx.sh
+++ infra/k8s/install-ingress-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install ingress-nginx ingress-nginx/ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx -f helm-ingress-nginx-overrides.yml

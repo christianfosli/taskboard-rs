e2ffe0152ececa6fa3e684ef89ff1d1729de3b3e
Christian Fosli <christian.fosli@capgemini.com>
2020-12-27 21:30:46 +0100

    Try to fix getting the wrong IP addr

--- infra/azure/terraform/dns.tf
+++ infra/azure/terraform/dns.tf
@@ -8,5 +8,5 @@ resource "azurerm_dns_a_record" "record" {
   zone_name           = azurerm_dns_zone.pubZone.name
   resource_group_name = data.azurerm_resource_group.rg.name
   ttl                 = 600
-  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.0.public_ip_address_id
+  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
 }

8885c038fe021d090996cb53274230fadcd23564
Christian Fosli <christian.fosli@capgemini.com>
2020-12-27 20:21:46 +0100

    Almost nothing

--- infra/k8s/terraform/ingress.tf
+++ infra/k8s/terraform/ingress.tf
@@ -5,11 +5,12 @@ resource "kubernetes_namespace" "ingressNamespace" {
 }
 
 resource "helm_release" "ingressNginx" {
-  name       = "ingress-nginx"
-  namespace  = kubernetes_namespace.ingressNamespace.metadata.0.name
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "~>3.17"
+  name        = "ingress-nginx"
+  namespace   = kubernetes_namespace.ingressNamespace.metadata.0.name
+  repository  = "https://kubernetes.github.io/ingress-nginx"
+  chart       = "ingress-nginx"
+  version     = "~>3.17"
+  max_history = 5
 
   set {
     name  = "controller.metrics.enabled"

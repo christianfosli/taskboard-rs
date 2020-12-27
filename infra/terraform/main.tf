a87057513e38b2193c7d008392d1183af3d536ba
Christian Fosli <christian.fosli@capgemini.com>
2020-12-27 11:36:42 +0100

    Add helm and kubectl provider

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,9 +5,25 @@ terraform {
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
+
+  required_providers {
+    azurerm = {
+      version = "~>2.41"
+    }
+
+    helm = {
+      version = "~>2.0"
+    }
+
+    kubectl = {
+      # Replace with official kubernetes provider when alpha version
+      # with CRD support matures
+      source  = "gavinbunney/kubectl"
+      version = "~>1.9"
+    }
+  }
 }
 
 provider "azurerm" {
-  version = "~>2.5"
   features {}
 }

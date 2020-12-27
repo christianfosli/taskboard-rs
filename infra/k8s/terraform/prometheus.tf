0a60de3c58155ede4a5129d5d5fd184708a23390
Christian Fosli <christian.fosli@capgemini.com>
2020-12-27 21:32:45 +0100

    Fix chartname

--- infra/k8s/terraform/prometheus.tf
+++ infra/k8s/terraform/prometheus.tf
@@ -8,7 +8,7 @@ resource "helm_release" "prometheus" {
   name        = "prometheus"
   namespace   = kubernetes_namespace.monitoring.metadata.0.name
   repository  = "https://prometheus-community.github.io/helm-charts"
-  chart       = "prometheus-stack"
+  chart       = "kube-prometheus-stack"
   version     = "~>12.8"
   max_history = 5
 

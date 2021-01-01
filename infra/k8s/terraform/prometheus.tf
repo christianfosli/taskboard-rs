fd710b2487223c31d756d2943129ab3dd7ec793a
Christian Fosli <christian.fosli@capgemini.com>
2021-01-01 20:49:04 +0100

    fixup! Make things a little prettier + Expose kibana through ingress

--- infra/k8s/terraform/prometheus.tf
+++ infra/k8s/terraform/prometheus.tf
@@ -24,7 +24,7 @@ YAML
 
 resource "kubectl_manifest" "ingressServiceMonitor" {
   yaml_body = templatefile("ingress-service-monitor.yaml", {
-    namespace         = kubernetes_namespace.monitor.metadata.0.name
+    namespace         = kubernetes_namespace.monitoring.metadata.0.name
     ingress_namespace = kubernetes_namespace.ingressNamespace.metadata.0.name
     release_name      = helm_release.prometheus.metadata.0.name
     ingress_app_name  = helm_release.ingressNginx.chart

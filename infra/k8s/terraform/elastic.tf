7c75ec892b4c2f9ce4d92c97ff354d2c360ebcd1
Christian Fosli <cfosli@gmail.com>
2021-01-10 18:31:32 +0100

    revert: "feat(infra): logging in K8S cluster with elastic" (#45)
    *Partially* reverts commit 018f6d6f45b53ad9bc2264b8ae158d774b786786.
    Running two nodes is a bit too expensive for my hobby project,
    and one B2s node is not enough RAM for this.

--- infra/k8s/terraform/elastic.tf
+++ infra/k8s/terraform/elastic.tf
@@ -21,20 +21,3 @@ resource "kubectl_manifest" "elasticSearchCluster" {
   yaml_body  = file("elasticsearch.yaml")
   depends_on = [helm_release.eckOperator]
 }
-
-resource "kubectl_manifest" "kibana" {
-  yaml_body = templatefile("kibana.yaml", {
-    elasticsearch_name = kubectl_manifest.elasticSearchCluster.name
-  })
-}
-
-resource "kubectl_manifest" "elasticBeats" {
-  yaml_body = templatefile("elastic-beats.yaml", {
-    elasticsearch_name = kubectl_manifest.elasticSearchCluster.name
-    kibana_name        = kubectl_manifest.kibana.name
-  })
-}
-
-resource "kubectl_manifest" "kibanaIngress" {
-  yaml_body = file("kibana-ingress.yaml")
-}

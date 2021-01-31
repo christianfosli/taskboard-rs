52c9fb533ea95f463a86651d9427adb2715a4a8d
Christian Fosli <christian.fosli@capgemini.com>
2021-01-31 14:49:56 +0100

    Revert "TEMP: comment out the DNS that requires nginx LB"
    This reverts commit d515fc6b382d79010ea28feb7ed984a585606402.

--- infra/azure/terraform/dns.tf
+++ infra/azure/terraform/dns.tf
@@ -3,26 +3,26 @@ resource "azurerm_dns_zone" "pubZone" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-#resource "azurerm_dns_a_record" "www" {
-#  name                = "www"
-#  zone_name           = azurerm_dns_zone.pubZone.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  ttl                 = 600
-#  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
-#}
-#
-#resource "azurerm_dns_a_record" "api" {
-#  name                = "api"
-#  zone_name           = azurerm_dns_zone.pubZone.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  ttl                 = 600
-#  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
-#}
-#
-#resource "azurerm_dns_a_record" "metrics" {
-#  name                = "metrics"
-#  zone_name           = azurerm_dns_zone.pubZone.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  ttl                 = 600
-#  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
-#}
+resource "azurerm_dns_a_record" "www" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.pubZone.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ttl                 = 600
+  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
+}
+
+resource "azurerm_dns_a_record" "api" {
+  name                = "api"
+  zone_name           = azurerm_dns_zone.pubZone.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ttl                 = 600
+  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
+}
+
+resource "azurerm_dns_a_record" "metrics" {
+  name                = "metrics"
+  zone_name           = azurerm_dns_zone.pubZone.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ttl                 = 600
+  target_resource_id  = data.azurerm_lb.aksloadbalancer.frontend_ip_configuration.1.public_ip_address_id
+}

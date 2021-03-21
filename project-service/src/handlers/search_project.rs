d320b6aaa0c94868b337925d4c37085dc4369d03
Christian Fosli <christian.fosli@capgemini.com>
2021-03-21 10:57:13 +0100

    fix(project-svc): percent decode search text and support spaces

--- project-service/src/handlers/search_project.rs
+++ project-service/src/handlers/search_project.rs
@@ -1,4 +1,5 @@
-use tracing::error;
+use percent_encoding::percent_decode_str;
+use tracing::{debug, error};
 use warp::{
     reject::{self, Reject},
     reply, Rejection, Reply,
@@ -10,6 +11,18 @@ pub async fn handle_search_project(
     store: impl ProjectStore,
     search_text: String,
 ) -> Result<impl Reply, Rejection> {
+    // decoding the search_text might be done automatically by warp in the future
+    // see https://github.com/seanmonstar/warp/issues/242
+    let search_text = percent_decode_str(&search_text)
+        .decode_utf8()
+        .map_err(|e| {
+            error!("Failed to decode search text: {}", e);
+            reject::custom(SearchProjectError {})
+        })?
+        .to_string();
+
+    debug!("Search text decoded to {}", &search_text);
+
     let projects = store.search(&search_text).await.map_err(|e| {
         error!(
             "Failed to get search results for {} from store: {}",

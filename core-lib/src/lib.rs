684bef94f456ff8c0bc76fb9664b2beb5aeca983
Christian Fosli <christian.fosli@capgemini.com>
2021-04-10 16:05:49 +0200

    feat(core-lib): add ErrorMessage type

--- core-lib/src/lib.rs
+++ core-lib/src/lib.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 mod project;
 pub use crate::project::*;
 pub use uuid;
@@ -6,3 +8,10 @@ mod task;
 pub use crate::task::*;
 
 pub mod commands;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+/// Errors from our back-end API should be in this format
+pub struct ErrorMessage {
+    pub message: String,
+    pub status_code: u16,
+}

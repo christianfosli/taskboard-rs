98b3d1daf78e74ce9997c20cf330d6bfb33dc806
Christian Fosli <christian.fosli@capgemini.com>
2022-02-19 18:57:50 +0100

    fixup! add fluentd to scrape container logs

--- infra/k8s/terraform/fluentd.tf
+++ infra/k8s/terraform/fluentd.tf
@@ -1,3 +1,27 @@
 resource "kubernetes_manifest" "fluentd" {
   manifest = yamldecode(file("fluentd.yaml"))
 }
+
+resource "random_password" "fluent" {
+  length           = 16
+  special          = true
+  override_special = "()-_%"
+}
+
+resource "kubernetes_secret" "taskboardEsFluentUser" {
+  metadata {
+    name      = "taskboard-es-fluent-user"
+    namespace = "kube-system"
+    labels = {
+      "k8s-app" = "fluentd-logging"
+    }
+  }
+
+  data = {
+    username = "fluent"
+    password = random_password.fluent.result
+  }
+}
+
+# MANUAL STEP: Create a user in elastic (i.e. through UI in Kibana)
+# that matches the credentials in the secret above

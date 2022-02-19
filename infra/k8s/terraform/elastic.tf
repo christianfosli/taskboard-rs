1d09c4e3b62480bdcf057793d63ac19de1733124
Christian Fosli <christian.fosli@capgemini.com>
2022-02-19 12:26:58 +0100

    chore: update elasticsearch and ECK

--- infra/k8s/terraform/elastic.tf
+++ infra/k8s/terraform/elastic.tf
@@ -9,7 +9,7 @@ resource "helm_release" "eckOperator" {
   namespace   = kubernetes_namespace.elasticNamespace.metadata.0.name
   repository  = "https://helm.elastic.co"
   chart       = "eck-operator"
-  version     = "1"
+  version     = "2"
   max_history = 5
 
   set {
